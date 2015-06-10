dc3bc2a7064f308bcd6ccd891a8913b3f63fb5e4
whogn020 <hong.wesley609@gmail.com>
2015-06-10 15:23:10 -0700

    player class

---
+++ src/Player.java
@@ -0,0 +1,42 @@
+/**
+ * Created by wesley on 2015-06-10.
+ */
+public class Player {
+    public static String summonerID ;
+    public static int age ;
+    public static String name ;
+    public static String nationality ;
+    public static float csPerGame ;
+    public static float goldPerMin ;
+    public static float kDA;
+
+
+    public String returnID() {
+        return summonerID ;
+    }
+
+    public int returnAge() {
+        return age;
+    }
+
+    public String returnName() {
+        return name ;
+    }
+
+    public String returnNationality() {
+        return nationality ;
+    }
+
+    public float returnCsPerGame() {
+        return csPerGame ;
+    }
+
+    public float returnGPM() {
+        return goldPerMin ;
+    }
+
+    public float returnKDA() {
+        return kDA ;
+    }
+
+}
