5f336ca96f7b8d613b38b49d5f36efd91c5b42d2
whogn020 <hong.wesley609@gmail.com>
2015-06-10 14:58:58 -0700

    gjhygjhjg

--- src/SearchFor.java
+++ src/SearchFor.java
@@ -1,11 +1,17 @@
-import javafx.geometry.Insets;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,33 +20,38 @@ import javafx.stage.Stage;
  */
 public class SearchFor {
 
+    // player attributes :
+    public static String summonerID ;
+    public static int age ;
+    public static String name ;
+    public static String nationality ;
+    public static float csPerGame ;
+    public static float goldPerMin ;
+    public static float kDA;
+
+
+    private static TextField tf = new TextField();
+
     public static void display() {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle("Search");
-        //window.setMinWidth(300);
+        window.setMinWidth(300);
 
-        TextField tf = new TextField();
         tf.setPromptText("Enter name");
         tf.setMinWidth(234);
 
         Button searchButton = new Button("Search");
 
-        ToggleGroup choices = new ToggleGroup();
-        RadioButton player = new RadioButton("Player");
-        RadioButton team = new RadioButton("Team");
-        RadioButton region = new RadioButton("Region");
-        player.setToggleGroup(choices);
-        team.setToggleGroup(choices);
-        region.setToggleGroup(choices);
-        player.setSelected(true);
+        CheckBox player = new CheckBox("Player");
+        CheckBox team = new CheckBox("Team");
+        CheckBox region = new CheckBox("Region");
 
         HBox searchBar = new HBox(5);
         searchBar.getChildren().addAll(tf, searchButton);
         searchBar.setAlignment(Pos.CENTER);
 
         GridPane layout = new GridPane();
-        layout.setVgap(5);
         Label selectCategories = new Label("Select Categories:");
         GridPane.setConstraints(selectCategories, 0, 0);
         GridPane.setConstraints(player, 0, 1);
@@ -49,38 +60,36 @@ public class SearchFor {
         layout.getChildren().addAll(selectCategories, player, team, region);
         layout.setAlignment(Pos.TOP_CENTER);
 
-        Label askReturn = new Label("Return data sorted by:");
-        ChoiceBox<String> attributes = new ChoiceBox<>();
-        attributes.getItems().addAll("summonerID", "Age", "Name", "KA/D Ratio", "csPerGame", "goldPerMin", "Nationality");
-        player.setOnAction(e -> {
-            attributes.getItems().clear();
-            attributes.getItems().addAll("summonerID", "Age", "Name", "KA/D Ratio", "csPerGame", "goldPerMin", "Nationality");})
-            ;
-        team.setOnAction(e-> {
-            attributes.getItems().clear();
-            attributes.getItems().addAll("Name", "Acronym", "Average Barons", "Average Dragons", "Wins", "Losses", "Sponsor");
-        });
-        region.setOnAction(e->{
-            attributes.getItems().clear();
-            attributes.getItems().addAll("Acronym", "Name");
-        });
+        BorderPane borderPane = new BorderPane(layout);
+        borderPane.setTop(searchBar);
 
-        GridPane.setConstraints(askReturn, 0, 3);
-        GridPane.setConstraints(attributes, 0, 4);
-        Text in = new Text("in");
-        GridPane.setConstraints(in, 0, 5);
-        ChoiceBox<String> order = new ChoiceBox<>();
-        order.getItems().addAll("", "Ascending order", "Descending order");
-        GridPane.setConstraints(order, 0, 6);
-
-        layout.getChildren().addAll(askReturn, attributes, in, order);
-
-            BorderPane borderPane = new BorderPane(layout);
-            borderPane.setTop(searchBar);
-            borderPane.setPadding(new Insets(10));
-            Scene scene = new Scene(borderPane);
+        Scene scene = new Scene(borderPane, 300, 100);
         window.setScene(scene);
         window.show();
 
+        // i guess i need the text field so i moved it to a class field
+
+    }
+
+    // going to execute the SQL query to find the player
+    public static void findPlayer(Connection con) throws SQLException {
+
+
+
+        Statement stmt = con.createStatement() ;
+        // find player is the result
+
+        // needs the box with the text field
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Player" + "WHERE summonerID =" + tf.getText() ) ;
+
+        while (rs.next())  {
+            summonerID = rs.getString("summonerID") ;
+            age = rs.getInt(2);
+            name = rs.getString(3) ;
+            nationality = rs.getString(4);
+            csPerGame = rs.getFloat(5) ;
+            goldPerMin = rs.getFloat(6) ;
+            kDA = rs.getFloat(7) ;
+        }
     }
 }
