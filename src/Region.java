77d34d9243fc9b4482beabc52ffff9db81c4986c
jchau623 <justin.chau623@gmail.com>
2015-06-10 15:55:09 -0700

    player-team-region

---
+++ src/Region.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Jason on 2015-06-10.
+ */
+public class Region {
+    public static String acronym;
+    public static String RegionName;
+
+    public String getRegionName(){
+        return RegionName;
+    }
+    public String getAcronym(){
+        return acronym;
+    }
+}
---
+++ src/Team.java
@@ -0,0 +1,43 @@
+
+
+/**
+ * Created by wesley on 2015-06-10.
+ */
+public class Team {
+
+    public static String teamName ;
+    public static int wins;
+    public static int losses;
+    public static String sponsor;
+    public static String acronym;
+    public static Float averageDragons;
+    public static Float averageBarons;
+
+    public String returnTeamName() {
+        return teamName;
+    }
+
+    public int returnWins(){
+        return wins ;
+    }
+
+    public int returnLosses(){
+        return losses ;
+    }
+    public String returnSponser(){
+        return sponsor;
+    }
+
+    public String returnAcronym(){
+        return acronym;
+    }
+
+    public  Float returnAvgD(){
+        return averageDragons ;
+    }
+
+    public Float returnB(){
+        return averageBarons ;
+    }
+}
+
