dc332a11cb01f6cf0de22052312cae4db6e7609d
jchau623 <justin.chau623@gmail.com>
2015-06-15 18:16:03 -0700

    headlines now start to show on check feed

---
+++ src/News.java
@@ -0,0 +1,23 @@
+/**
+ * Created by jch on 15/06/15.
+ */
+public class News {
+    private String headline;
+    private String URL;
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
