0ba575364bfc73813e373a22122aceb4a599a18c
Vivian Jin <vivianjin95@gmail.com>
2015-06-17 02:21:22 -0700

    added ViewUsers (division)

--- src/Main.java
+++ src/Main.java
@@ -336,8 +336,11 @@ public class Main extends Application {
         Button button5 = new Button("View Matches");
         button5.setOnAction(e -> ViewMatches.display(con, "View Matches", "View Matches By Team"));
 
+        Button button6 = new Button("View Our Top Users!");
+        button6.setOnAction(e ->ViewUsers.display(con, "View Our Top Users", "View Our Top Users"));
+
         VBox layout = new VBox(20);
-        layout.getChildren().addAll(message, userAndButton, button1, button4, button5, button3);
+        layout.getChildren().addAll(message, userAndButton, button1, button4, button5, button6, button3);
         layout.setAlignment(Pos.CENTER);
         scene1 = new Scene(layout, 300, 500);
 
