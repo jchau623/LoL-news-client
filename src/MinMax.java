2386415bf455e0559e62235f6421420fa798824a
jchau623 <justin.chau623@gmail.com>
2015-06-17 01:58:22 -0700

    how do i get min(avg(barons))

--- src/MinMax.java
+++ src/MinMax.java
@@ -32,12 +32,26 @@ public class MinMax {
 
         Statement getTeams = con.createStatement();
         ResultSet teams = getTeams.executeQuery("SELECT name FROM TeamThatPlaysIn");
+        teams.next();
+        guess.getItems().add(teams.getString("name"));
+        guess.setValue(teams.getString("name"));
         while (teams.next()) {
             guess.getItems().add(teams.getString("name"));
         }
 
+
         Button minButton = new Button("Min");
         Button maxButton = new Button("Max");
+
+        minButton.setOnAction(e -> {
+            try {
+                double team = getTeamAvgBaron(con, guess.getValue());
+                double min = getMinAvgBaron(con);
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        });
+
         HBox buttons = new HBox(20);
         buttons.setAlignment(Pos.CENTER);
         buttons.setPadding(new Insets(5));
@@ -49,5 +63,29 @@ public class MinMax {
         Scene scene = new Scene(bp);
         window.setScene(scene);
         window.showAndWait();
+    }
+
+    private static double getMinAvgBaron(Connection con) {
+
+    }
+
+    private static double getTeamAvgBaron(Connection con, Object value) throws SQLException {
+        Statement sumRedBaron = con.createStatement();
+        ResultSet sumRBRS = sumRedBaron.executeQuery("SELECT SUM(redNumOfBarons) AS redBaronSum FROM Match m " +
+                "WHERE m.redName = '" + value + "'");
+        sumRBRS.next();
+        float redBaronSum = sumRBRS.getInt(1);
+        Statement sumBlueBaron = con.createStatement();
+        ResultSet sumBBRS = sumBlueBaron.executeQuery("SELECT SUM(blueNumOfBarons) AS blueBaronSum FROM Match m " +
+                "WHERE m.blueName = '" + value + "'");
+        sumBBRS.next();
+        float blueBaronSum = sumBBRS.getInt(1);
+        Statement matchCount = con.createStatement();
+        ResultSet tm = matchCount.executeQuery("SELECT COUNT(*) FROM Match m WHERE m.redname = '" + value + "' OR " +
+                "m.bluename = '" + value +"'");
+        tm.next();
+        float totalMatches = tm.getInt(1);
+        float averageBaron = (redBaronSum+blueBaronSum)/totalMatches;
+        return averageBaron;
     }
 }
